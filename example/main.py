fb023c3893a0beff3fa86e103aa0130fae2c374a
Morgan Trexler <trexler2000@gmail.com>
2024-02-03 19:02:22 -0700

    made some basic improvements

--- example/main.py
+++ example/main.py
@@ -1,6 +1,13 @@
 from machine import ADC
 from thermistor import Thermistor
+import time
 
 therm = Thermistor(ADC(4, atten=ADC.ATTN_11DB), beta=3435, therm_ohm=10_000, divider_ohm=10_000)
 
-print(therm.read_temperature_fahrenheit())
+while True:
+    print("Measured Voltage:      " + str(therm.read_voltage()) + " V")
+    print("Measured Resistance:   " + str(therm.read_resistance()) + " Ohm")
+    print("Measured Temperature:  " + str(therm.read_temperature_celsius()) + " C")
+    print("")
+    time.sleep(2)
+
